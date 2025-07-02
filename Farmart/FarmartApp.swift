32f80ca0e077b52fb5c436f14ff620085e263efc
Anubhav <programmingwithanubhav@gmail.com>
2025-07-02 12:28:22 +0530

    Initial Commit

---
+++ Farmart/FarmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmartApp.swift
+//  Farmart
+//
+//  Created by Batch  - 2 on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FarmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
